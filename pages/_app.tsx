00de58b6d69258a2ea7fbe2b10ad4a2119da1ef8
Prince Anuragi <princanurag07@gmail.com>
2022-11-25 18:10:55 +0530

    Added Chakra UI as UI Kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import { Web3ReactProvider } from "@web3-react/core";
 import type { AppProps } from "next/app";
 import getLibrary from "../getLibrary";
-import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme";
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+    <ChakraProvider theme={theme}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
+    </ChakraProvider>
   );
 }
 
